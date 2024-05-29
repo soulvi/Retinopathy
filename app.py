073c9812959d3a62441a203d5df02522f523cb6d
LevShlyogin <147638117+LevShlyogin@users.noreply.github.com>
2024-05-29 17:01:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -39,34 +39,3 @@ def predict(image):
         probabilities = torch.nn.functional.softmax(outputs, dim=1)
         _, predicted = torch.max(outputs, 1)
     return predicted.item(), probabilities.squeeze().cpu().numpy()
-
-# Интерфейс Streamlit
-st.title("Классификация степени заболевания")
-
-uploaded_file = st.file_uploader("Загрузите изображение", type=["jpg", "jpeg", "png"])
-
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Загруженное изображение", use_column_width=True)
-    
-    # Предсказание сразу после загрузки изображения
-    label, probabilities = predict(image)
-    
-    # Проверка вероятности предсказания
-    min_probability = 0.5
-    if max(probabilities) < min_probability:
-        st.error("Ошибка: Максимальная вероятность предсказания меньше 0.5.")
-    else:
-        st.write(f"Предсказанный класс: {label}")
-        if label == 0:
-            st.write("Класс 0: Здоровый")
-        elif label == 1:
-            st.write("Класс 1: Stage 2-3")
-        elif label == 2:
-            st.write("Класс 2: Plus")
-        
-        # Вывод вероятностей для каждого класса
-        st.write("Вероятности классов:")
-        st.write(f"Здоровый: {probabilities[0]:.4f}")
-        st.write(f"Stage 2-3: {probabilities[1]:.4f}")
-        st.write(f"Plus: {probabilities[2]:.4f}")
